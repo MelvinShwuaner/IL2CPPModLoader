b0fa7da3ef222cc73f1a0b181d4479ed0a0bfcf6
Melvin "Dumbass" Shwuaner <83349478+MelvinShwuaner@users.noreply.github.com>
2026-06-04 00:04:33 +0300

    mhm

--- Native/Android/libDotNetPlugin/library.cpp
+++ Native/Android/libDotNetPlugin/library.cpp
@@ -1,5 +1,3 @@
-
-#include <stdio.h>
 #include <assert.h>
 #include <chrono>
 #include <iostream>
--- Native/Android/libDotNetPlugin/library.h
+++
@@ -1,9 +0,0 @@
-#ifndef HOSTER_LIBRARY_H
-#define HOSTER_LIBRARY_H
-
-int LoadHostFxr(const char* RuntimeConfigPath,
-        const char* DotNetPath,
-        const char* EntryPointType,
-        const char* EntryPointFunction);
-
-#endif // HOSTER_LIBRARY_H
