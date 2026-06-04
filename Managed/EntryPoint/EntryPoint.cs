016e69b4cf8755aadbf37bdd2e06be52a3482c43
Melvin "Dumbass" Shwuaner <83349478+MelvinShwuaner@users.noreply.github.com>
2026-06-04 10:17:40 +0300

    sz

--- Managed/EntryPoint/EntryPoint.cs
+++ Managed/EntryPoint/EntryPoint.cs
@@ -1,11 +1,14 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 public static class EntryPoint
 {
+    public static string MainPath;
     [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvCdecl) })]
     public static void Init()
     {
        DotNet.Log("Hello World!");
+       MainPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
     }
 }
--- Managed/ModLoader/ModLoader/ModLoader.cs
+++ Managed/ModLoader/ModLoader/ModLoader.cs
@@ -1,4 +1,20 @@
 ﻿public class ModLoader
 {
-    
+    public static void Prepare()
+    {
+        
+    }
+}
+
+public class Il2CppAssemblyGenerator
+{
+    static string MainPath => EntryPoint.MainPath + "/Il2CppAssemblies";
+    public static void Prepare()
+    {
+        if (Path.Exists(MainPath))
+        {
+            return;
+        }
+        
+    }
 }
